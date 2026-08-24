59e9cef84858bc1b75ec932dd4667d7ac32841ef
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-24 15:35:00 +0500

    client only

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -4,15 +4,18 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { clientSideFunction } from '@/utils/client.utils'
 
 
 export default function ClientRoute() {
-    const settings = {
-        dots: true,
-    };
+
+    const result = clientSideFunction();
+    // const settings = {
+    //     dots: true,
+    // };
     return (
         <div className="image-slider-container">
-            <Slider {...settings}>
+            {/* <Slider {...settings}>
                 <div>
                     <img src="https://images.pexels.com/photos/39062564/pexels-photo-39062564/free-photo-of-black-and-white-photo-of-humayun-s-tomb-new-delhi.jpeg?auto=compress&w=1200" />
                 </div>
@@ -25,7 +28,8 @@ export default function ClientRoute() {
                 <div>
                     <img src="https://images.pexels.com/photos/39104444/pexels-photo-39104444/free-photo-of-black-and-white-geometry-with-flying-bird.jpeg?auto=compress&w=1200" />
                 </div>
-            </Slider>
+            </Slider> */}
+            <p>{result}</p>
         </div>
     );
 }
---
+++ rendering-demo/src/components/server-component-one.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+
+export const ServerComponentone = () => {
+    fs.readFileSync("src/components/server-component-one.tsx", "utf-8")
+    return 
+}
