c251ef2927e2ff03963b8724274acdf8efdfcdf2
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-24 16:11:58 +0500

    interleaving

---
+++ rendering-demo/src/app/interleaving/page.tsx
@@ -0,0 +1,18 @@
+// import { ServerComponentOne } from "@/components/server-component-one";
+// import { ClientComponentOne } from "@/components/client-component-one";
+import { ClientComponentOne } from "@/components/client-component-one";
+import { ServerComponentOne } from "@/components/server-component-one";
+
+
+export default function InterLeavingPage() {
+    return (
+        <>
+            <h1>Interleaving page</h1>
+            <ClientComponentOne >
+                <ServerComponentOne />
+            </ClientComponentOne>
+            {/* <ServerComponentOne /> */}
+            {/* <ClientComponentOne /> */}
+        </>
+    )
+}
---
+++ rendering-demo/src/components/client-component-one.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { ServerComponentOne } from "./server-component-one";
+
+import { useState } from "react"
+
+
+export const ClientComponentOne = ({ children }: { children: React.ReactNode }) => {
+    const [name, setName] = useState("Batman")
+    return (
+        <>
+            <h1> Client component one </h1>
+            {children}
+        </>
+    )
+}
