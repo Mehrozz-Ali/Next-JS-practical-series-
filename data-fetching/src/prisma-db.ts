e3adc57dea7acb37d5a91c29036c3963bc034edf
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-25 14:35:26 +0500

    datafetching from database error fixed

--- data-fetching/src/prisma-db.ts
+++ data-fetching/src/prisma-db.ts
@@ -3,7 +3,7 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 
 const adapter = new PrismaBetterSqlite3({
-    url: "file:./app.db",
+    url: process.env.DATABASE_URL!,
 });
 
 const prisma = new PrismaClient({ adapter });
