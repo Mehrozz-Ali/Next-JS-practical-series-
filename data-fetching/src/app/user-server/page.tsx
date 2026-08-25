8f7b03144dbb8350761923fdfaecdae570dab4ac
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-25 09:46:12 +0500

    handling error & loading in data fetching

--- data-fetching/src/app/user-server/page.tsx
+++ data-fetching/src/app/user-server/page.tsx
@@ -1,5 +1,3 @@
-"use client"
-import { useState, useEffect } from "react";
 
 type User = {
     id: number;
@@ -12,6 +10,7 @@ type User = {
 
 
 export default async function UserServer() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users: User[] = await response.json();
 
