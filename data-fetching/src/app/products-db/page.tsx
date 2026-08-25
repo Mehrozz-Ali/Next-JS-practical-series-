922b805fd947210b9901fb3a12bfc7bf31a00899
Mehrozz-Ali <mehrozali912@gmail.com>
2026-08-25 18:13:01 +0500

    crud operation complete

--- data-fetching/src/app/products-db/page.tsx
+++ data-fetching/src/app/products-db/page.tsx
@@ -1,7 +1,8 @@
 import { getProducts } from "@/prisma-db";
+import ProductDetail from "./product-detail";
 
 
-type Product = {
+export type Product = {
     id: number;
     title: string;
     price: number;
@@ -12,17 +13,8 @@ type Product = {
 export default async function ProductsDBPage() {
     const products: Product[] = await getProducts();
 
-    return (
-        <ul className="space-y-4 p-4">
-            {products.map((product) => (
-                <li key={product.id} className='p-4 bg-white shadow-md rounded-lg text-gray-700'>
-                    <h2 className="text-xl font-semibold">{product.title}</h2>
-                    <p>{product.description}</p>
-                    <p className="text-lg font-medium">${product.price.toFixed(2)}</p>
-                </li>
-            ))
-            }
-        </ul>
-    )
+
+
+    return <ProductDetail products={products} />;
 
 }
